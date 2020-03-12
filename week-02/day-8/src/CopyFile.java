f16427739c508e5befa896ec03f699c32e1c01ab
LRobin92 <laszlorobin@gmail.com>
2020-03-12 15:50:58 +0100

    Graphics excercises

--- week-02/day-8/src/CopyFile.java
+++ week-02/day-8/src/CopyFile.java
@@ -1,18 +1,32 @@
+import java.io.*;
+import java.nio.Buffer;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class CopyFile {
     public static void main(String[] args) {
         try{
-
+            fileCopier();
         }catch(Exception e){
-
+            System.out.println("Something went wrong");
         }
         // Write a function that copies the contents of a file into another
 // It should take the filenames as parameters
 // It should return a boolean that shows if the copy was successful
     }
-    public static void fileCopier(){
-        Path filePath = Paths.get("my-file.txt");
+    public static void fileCopier() throws IOException {
+        Path filePath = Paths.get("my-file.txt"); //create a Path object
+        FileReader fileReader = new FileReader("my-file.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        FileWriter fileWriter = new FileWriter("copyofmyfile.txt");
+        String copyString;
+
+        while((copyString =bufferedReader.readLine()) != null){
+            fileWriter.write(copyString);
+            fileWriter.flush();
+        }
+        bufferedReader.close();
+        fileWriter.close();
     }
 }
--- week-02/day-8/src/WriteSingleLine.java
+++ week-02/day-8/src/WriteSingleLine.java
@@ -2,9 +2,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 public class WriteSingleLine {
     public static void main(String[] args) {
