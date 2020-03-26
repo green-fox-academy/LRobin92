9dcbf5011bc6928f06b5553a917254921f7b8c8f
LRobin92 <laszlorobin@gmail.com>
2020-03-26 17:12:28 +0100

    Finished the Pirate class

---
+++ week-04/day-4/src/pirates/BattleApp.java
@@ -0,0 +1,13 @@
+package pirates;
+
+public class BattleApp {
+    public static void main(String[] args) {
+        Pirate pirate1 = new Pirate("BigBeard");
+        Pirate pirate2 = new Pirate("Lil Rat");
+
+
+        pirate1.drinkSomeRum();
+
+
+    }
+}
