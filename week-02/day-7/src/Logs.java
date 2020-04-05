a360d03dc791536b589156ad178ab730c7c65b92
LRobin92 <laszlorobin@gmail.com>
2020-04-05 19:06:00 +0200

    Done with the logs excercise

--- week-02/day-7/src/Logs.java
+++ week-02/day-7/src/Logs.java
@@ -10,12 +10,11 @@ import java.util.List;
 import java.util.Map;
 
 public class Logs {
-  // Write a function that returns an array with the unique IP adresses.
-// Write a function that returns the GET / POST request ratio.
   public static void main(String[] args) {
     System.out.println(uniqueIPAdresses());
+    System.out.println(ratioGetPost());
   }
-
+  // Write a function that returns an array with the unique IP adresses.
   public static List<String> uniqueIPAdresses() {
     List<String> lines = new ArrayList<>();
     HashMap<String , Integer> uniqeIPAdressesHashMap = new HashMap<>();
@@ -45,4 +44,36 @@ public class Logs {
     }
     return uniqeIPAdresses;
   }
+  // Write a function that returns the GET / POST request ratio.
+  public static int ratioGetPost(){
+    int getCounter = 0;
+    int postCounter = 0;
+    List<String> lines = new ArrayList<>();
+    Path filePath = Paths.get("log.txt");
+    try {
+      lines = Files.readAllLines(filePath);
+    } catch (NoSuchFileException e) {
+      System.out.println("No such file");
+    } catch (IOException e) {
+      System.out.println("Unable to read the file");
+    } catch (Exception e) {
+      System.out.println("Something went wrong");
+    }
+    List<String> getAndPost = new ArrayList<>();
+    for (String line : lines) {
+      String[] splitLines = line.split("   ");
+      getAndPost.add(splitLines[2]);
+    }
+    for (String word: getAndPost) {
+      if (word.equals("POST /")){
+        postCounter++;
+      }else if(word.equals("GET /")){
+        getCounter++;
+      }
+    }
+    int ratio = getCounter / postCounter;
+    System.out.println(getAndPost);
+    return ratio;
+  }
+
 }
