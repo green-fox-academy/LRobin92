4cd6c6356161482dd45499421935c4fdeed1ea2a
LRobin92 <laszlorobin@gmail.com>
2020-04-06 12:23:57 +0200

    Done with the duplicate task

---
+++ week-02/day-7/src/Doubled.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Doubled {
+  public static void main(String[] args) {
+    // Create a method that decrypts the duplicated-chars.txt
+    System.out.println(decryptFunction());
+    //decryptFunction();
+
+  }
+  protected static List<String> decryptFunction(){
+    List<String> lines = new ArrayList<>();
+    List<String> theDecryptedList = new ArrayList<>();
+
+    Path filePath = Paths.get("duplicated-chars.txt");
+    try {
+      lines = Files.readAllLines(filePath);
+    } catch (NoSuchFileException e) {
+      System.out.println("No such file");
+    } catch (IOException e) {
+      System.out.println("Unable to read the file");
+    } catch (Exception e) {
+      System.out.println("Something went wrong");
+    }
+    for (String line: lines) {
+      List<Character> theDuplicatedChars = new ArrayList<>();
+      List<Character> theDecryptedChars = new ArrayList<>();
+      String charsBackToString = "";
+      for (char c: line.toCharArray()) {
+        theDuplicatedChars.add(c);
+      }
+      for (int i = 0; i < theDuplicatedChars.size(); i++) {
+        if (i > 1) {
+          theDuplicatedChars.remove(i - 1);
+        }
+        theDecryptedChars.add(theDuplicatedChars.get(i));
+      }
+      for (int i = 1; i < theDecryptedChars.size() ; i++) {
+        charsBackToString +=theDecryptedChars.get(i);
+      }
+      theDecryptedList.add(charsBackToString);
+    }
+    return  theDecryptedList;
+  }
+}
