41bb132ec568dcf2a539c04a759ce42e46a3fff0
LRobin92 <laszlorobin@gmail.com>
2020-04-18 17:40:52 +0200

    Done some menu options, avarage still left

--- week-06/day-05/src/main/java/com/greenfoxacademy/webshop/controllers/MainController.java
+++ week-06/day-05/src/main/java/com/greenfoxacademy/webshop/controllers/MainController.java
@@ -2,20 +2,17 @@ package com.greenfoxacademy.webshop.controllers;
 
 import com.greenfoxacademy.webshop.ShopItem;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-
-  @RequestMapping(value="/webshop", method = RequestMethod.GET) //
-  public String displayMyShop(Model model){
-    model.addAttribute("items", shopItemsList);
-    return "myshop";
-  }
   private List<ShopItem> emptyList = new ArrayList<>();
   private List<ShopItem> shopItemsList = addItemsToList();
 
@@ -32,6 +29,49 @@ public class MainController {
         300,1));
     return emptyList;
   }
+
+  @ResponseBody
+  @RequestMapping(value = "", method = RequestMethod.GET)
+  public String helloWorld() {
+    return "Hello World";
+  }
+  @RequestMapping(value="/webshop", method = RequestMethod.GET) //
+  public String displayMyShop(Model model){
+    model.addAttribute("items", shopItemsList);
+    return "myshop";
+  }
+  @RequestMapping(value = "webshop/onlyavaible", method = RequestMethod.GET)
+  public String onlyAvaible(Model model) {
+    model.addAttribute("items", shopItemsList.stream()
+        .filter(item -> item.getQuantityOfStock() > 0)
+        .collect(Collectors.toList()));
+    return "myshop";
+  }
+  @RequestMapping(value = "webshop/cheapest", method = RequestMethod.GET)
+  public String cheapestFirst(Model model) {
+    model.addAttribute("items", shopItemsList.stream()
+        .sorted(Comparator.comparing(ShopItem::getPrice))
+        .collect(Collectors.toList()));
+    return "myshop";
+  }
+  @RequestMapping(value = "webshop/containsnike", method = RequestMethod.GET)
+  public String containsNike(Model model) {
+    model.addAttribute("items", shopItemsList.stream()
+        .filter(item -> item.getName().toLowerCase().contains("nike")
+            || item.getDescription().toLowerCase().contains("nike"))
+        .collect(Collectors.toList()));
+    return "myshop";
+  }
+  @RequestMapping(value = "webshop/mostexpensive", method = RequestMethod.GET)
+  public String mostExpensiveItem(Model model) {
+    model.addAttribute("items", shopItemsList.stream()
+        .sorted(Comparator.comparingDouble(ShopItem::getPrice).reversed())
+        .limit(1)
+        .collect(Collectors.toList()));
+    return "myshop";
+  }
+
+
 
 
 
