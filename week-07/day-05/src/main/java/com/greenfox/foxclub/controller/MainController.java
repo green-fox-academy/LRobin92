b2f556f62577fc175aceb1145c75ab451f207fe6
LRobin92 <laszlorobin@gmail.com>
2020-04-24 16:52:00 +0200

    Did some upgrades on the classes

--- week-07/day-05/src/main/java/com/greenfox/foxclub/controller/MainController.java
+++ week-07/day-05/src/main/java/com/greenfox/foxclub/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.greenfox.foxclub.controller;
 
 import com.greenfox.foxclub.models.Fox;
+import com.greenfox.foxclub.services.FoxService;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -16,15 +17,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
   private Fox fox;
+  private FoxService service;
   private List<Fox> listOfGreenFoxes = new ArrayList<>();
 
+
   @Autowired
-  public MainController(Fox fox) {
+  public MainController(Fox fox, FoxService service) {
     this.fox = fox;
+    this.service = service;
   }
 
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public String displayMainPage(Model model) {
+    model.addAttribute("list", service.getFoxes().size());
     model.addAttribute("foxinformation", "This is " + fox.getName()
         + ". Currently living on " + fox.getFood() + " and " + fox.getDrink() +
         ". He knows " + fox.getListOfTrick().size() + " tricks.");
@@ -44,11 +49,11 @@ public class MainController {
 
   @RequestMapping(path = "/login", method = RequestMethod.POST)
   public String postLogin(@RequestParam(value = "nameInput") String nameInput, Model model) {
-    if (listOfGreenFoxes.stream().noneMatch(fox -> fox.getName().contains(nameInput))) {
-      fox.setName(nameInput);
-      listOfGreenFoxes.add(fox);
+    if (service.getFoxes().stream().noneMatch(fox -> fox.getName().contains(nameInput))) {
+      service.addFox(fox);
+      this.fox.setName(nameInput);
       return "redirect:/?name=" + nameInput;
-    } else if (listOfGreenFoxes.stream().anyMatch(fox -> fox.getName().contains(nameInput))) {
+    } else if (service.getFoxes().stream().anyMatch(fox -> fox.getName().contains(nameInput))) {
       return "redirect:/?name=" + nameInput;
     } else {
       return null;
@@ -63,6 +68,23 @@ public class MainController {
   @RequestMapping(value = "/nutritionStore", method = RequestMethod.GET)
   public String displayNutriStore() {
     return "nutristore";
+  }
+  @RequestMapping(value = "/trickCenter", method = RequestMethod.GET)
+  public String displayTricksCenter() {
+    return "trickscenter";
+  }
+  @RequestMapping(value = "/trickCenter", method = RequestMethod.POST)
+  public String learnTricks(int numberoftricks, Model model) {
+    if (numberoftricks == 1){
+      fox.getListOfTrick().add("Programming in Java");
+    }else if(numberoftricks == 2){
+      fox.getListOfTrick().add("Write in HTML");
+    }else if (numberoftricks == 3){
+      fox.getListOfTrick().add("Giving great presentation");
+    } else{
+      fox.getListOfTrick().add("Can speak english fluently");
+    }
+    return "redirect:/?name=" + fox.getName();
   }
 
 
--- week-07/day-05/src/main/java/com/greenfox/foxclub/services/FoxService.java
+++ week-07/day-05/src/main/java/com/greenfox/foxclub/services/FoxService.java
@@ -1,7 +1,23 @@
 package com.greenfox.foxclub.services;
 
+import com.greenfox.foxclub.models.Fox;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FoxService {
+  private List<Fox> foxes;
+
+  public FoxService(){
+    foxes = new ArrayList<>();
+  }
+
+  public List<Fox> getFoxes(){
+    return foxes;
+  }
+
+  public void addFox(Fox fox){
+    foxes.add(fox);
+  }
 }
