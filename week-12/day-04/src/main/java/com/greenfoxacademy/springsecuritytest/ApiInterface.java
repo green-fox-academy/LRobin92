38d782c1670723a55425a761758a69192206dca8
LRobin92 <laszlorobin@gmail.com>
2020-05-28 18:29:25 +0200

    Added interface, and models to the project

--- week-12/day-04/src/main/java/com/greenfoxacademy/springsecuritytest/ApiInterface.java
+++ week-12/day-04/src/main/java/com/greenfoxacademy/springsecuritytest/ApiInterface.java
@@ -1,4 +1,22 @@
 package com.greenfoxacademy.springsecuritytest;
 
-public class ApiInterface {
+import com.greenfoxacademy.springsecuritytest.models.MovieResults;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+  @GET("/3/movie/{category}")
+  Call<MovieResults> listOfMovies(
+      @Path("category") String category,
+      @Query("api_key") String apiKey,
+      @Query("language") String language,
+      @Query("page") int page
+  );
+
+
+
+  //https://api.themoviedb.org/3/movie/popular?api_key=bf97d990f803c4a7dff6c49860ab83ff&language=en-US&page=3
 }
--- week-12/day-04/src/main/java/com/greenfoxacademy/springsecuritytest/SpringsecuritytestApplication.java
+++ week-12/day-04/src/main/java/com/greenfoxacademy/springsecuritytest/SpringsecuritytestApplication.java
@@ -2,12 +2,24 @@ package com.greenfoxacademy.springsecuritytest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @SpringBootApplication
-public class SpringsecuritytestApplication {
+public class SpringsecuritytestApplication  {
+
+  
+  public static String BASE_URL = "https://developers.themoviedb.org";
 
   public static void main(String[] args) {
     SpringApplication.run(SpringsecuritytestApplication.class, args);
+
+    Retrofit retrofit = new Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build();
+
+    ApiInterface apiInterface = retrofit.create(ApiInterface.class);
   }
 
 }
